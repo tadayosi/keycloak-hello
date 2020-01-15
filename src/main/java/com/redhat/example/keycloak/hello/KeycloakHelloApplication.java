521f2acbd6a83c7173685d22b28c8e8dc5822be5
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2020-01-15 17:50:07 +0900

    initializr

---
+++ src/main/java/com/redhat/example/keycloak/hello/KeycloakHelloApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.example.keycloak.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redhat/example/keycloak/hello/KeycloakHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.example.keycloak.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
